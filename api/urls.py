a9d60314a9ec5bdd8df54a4793d3b86a82ca5f33
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-19 11:02:53 +0545

    viewsets

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('employees', views.EmployeeViewSet, basename='employee')
 
 urlpatterns = [
     path('students/', views.studentsView),
     path('students/<int:pk>/', views.studentDetailView, name="student-detail"),
 
-    path('employees/', views.Employees.as_view(), name="employee-list"),
-    path('employees/<int:pk>/', views.EmployeeDetail.as_view(), name="employee-detail"),
+    # path('employees/', views.Employees.as_view(), name="employee-list"),
+    # path('employees/<int:pk>/', views.EmployeeDetail.as_view(), name="employee-detail"),
+
+    path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -6,7 +6,8 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from employees.models import Employee
 from django.http import Http404
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, viewsets
+from django.shortcuts import get_object_or_404
 
 
 @api_view(['GET', 'POST'])
@@ -117,12 +118,48 @@ def studentDetailView(request, pk):
 #     def delete(self, request, *args, **kwargs):
 #         return self.destroy(request, *args, **kwargs)
 
+#Generics
+# class Employees(generics.ListCreateAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
 
-class Employees(generics.ListCreateAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+# class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+#     lookup_field = 'pk'
 
-class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-    lookup_field = 'pk'
+
+#Viewsets
+
+class EmployeeViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Employee.objects.all()
+        serializer = EmployeeSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None):
+        employee = get_object_or_404(Employee, pk=pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def update(self, request, pk=None):
+        employee = get_object_or_404(Employee, pk=pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk=None):
+        employee = get_object_or_404(Employee, pk=pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    
