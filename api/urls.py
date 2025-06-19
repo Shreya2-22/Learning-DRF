732035018b615c97f61a65b56ca9e3711b1d0df3
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-19 10:29:17 +0545

    make class based views

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('students/', views.studentsView),
     path('students/<int:pk>/', views.studentDetailView, name="student-detail"),
+
+    path('employees/', views.Employees.as_view(), name="employee-list"),
+    path('employees/<int:pk>/', views.EmployeeDetail.as_view(), name="employee-detail"),
 ]
